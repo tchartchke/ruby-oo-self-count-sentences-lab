d6c5e4231cea310f5360c913379e981b7581ad84
tchartchke <chartai.kl@gmail.com>
2020-10-14 18:59:17 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    size = self.split(/[?!.][\s]/).size
+    size ? size : 0
   end
 end
